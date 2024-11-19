23d54ee868773cfb3e762ab17bb59b3869a9e19e
Evina88 <evinamous@gmail.com>
2024-11-19 15:08:40 +0200

    create patient

--- app/Filament/Resources/PatientResource.php
+++ app/Filament/Resources/PatientResource.php
@@ -22,28 +22,83 @@ class PatientResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                //
-            ]);
+            ->schema(
+                [
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Select::make('type')
+                    ->options(
+                        [
+                        'cat' => 'Cat',
+                        'dog' => 'Dog',
+                        'rabbit' => 'Rabbit',
+                        ]
+                    )
+                    ->required(),
+                Forms\Components\DatePicker::make('date_of_birth')
+                    ->required()
+                    ->maxDate(now()),
+                Forms\Components\Select::make('owner_id')
+                // The first argument of the relationship() method is the name of the function that defines the relationship in the model
+                //(used by Filament to load the select options) .
+                //The second argument is the column name to use from the related table.
+                    ->relationship('owner', 'name')
+                    ->searchable()
+                    // preload() the first 50 owners into the searchable list (in case the list is long)
+                    ->preload()
+                    ->createOptionForm(
+                        [
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('email')
+                        // label() overrides the auto-generated label for each field
+                            ->label('Email address')
+                            // email() ensures that only valid email addresses can be input into the field.
+                            // It also changes the keyboard layout on mobile devices.
+                            ->email()
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('phone')
+                            ->label('Phone number')
+                            // tel() ensures that only valid phone numbers can be input into the field. It also changes the keyboard layout on mobile devices.
+                            ->tel()
+                            ->required(),
+                        ]
+                    )
+                    ->required()
+                ]
+            );
     }
 
     public static function table(Table $table): Table
     {
         return $table
-            ->columns([
+            ->columns(
+                [
                 //
-            ])
-            ->filters([
+                ]
+            )
+            ->filters(
+                [
                 //
-            ])
-            ->actions([
+                ]
+            )
+            ->actions(
+                [
                 Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
+                ]
+            )
+            ->bulkActions(
+                [
+                Tables\Actions\BulkActionGroup::make(
+                    [
                     Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
+                    ]
+                ),
+                ]
+            );
     }
 
     public static function getRelations(): array
